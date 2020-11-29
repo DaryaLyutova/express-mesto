39f2030c704113a1413dc8efd51af7f9051e1e27
Darya Lutuva <lutowa.darya@yandex.ru>
2020-11-29 21:28:35 +0300

    feat: ответ при несуществующем адресе

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const PORT = 3000;
 
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
+const errorRouter = require('./routes/errorUrl');
 
 // const dataCardsPath = path.join(__dirname, '..', 'data', 'cards.json');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', cardsRouter);
 app.use('/', usersRouter);
+app.use('/', errorRouter);
 
 app.listen(PORT, () => {
   console.log(`On port ${PORT}`);
