395ee1b0edcf64dc858fb390ea1246dff015d4ea
Darya Lutuva <lutowa.darya@yandex.ru>
2020-11-29 21:18:35 +0300

    feat: настройка роутера на id пользователя

--- controles/users.js
+++ controles/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 const path = require('path');
 const getDataInfo = require('../helpers/helpers');
 
@@ -13,10 +14,19 @@ const getUsers = (req, res) => {
 // eslint-disable-next-line arrow-body-style
 const getUser = (req, res) => {
   return getDataInfo(dataUsersPath)
+    // eslint-disable-next-line arrow-body-style
     .then((users) => {
-      res.status(200).send(users.id);
+      return users.find((user) => (user._id === req.params.id));
     })
-    .catch(() => { res.status(400).send({ error: 'Такого пользователя нет' }); });
+    .then((user) => {
+      if (!user) {
+        // eslint-disable-next-line quote-props
+        return res.status(404).send(JSON.parse({ "message": "Нет пользователя с таким id" }));
+      }
+      return res.status(200).send(user);
+    })
+    // eslint-disable-next-line quote-props
+    .catch(() => { res.status(400).send({ "message": "Что-то пошло не так" }); });
 };
 
 module.exports = { getUsers, getUser };
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -3,8 +3,10 @@ const fsPromises = require('fs').promises;
 // eslint-disable-next-line arrow-body-style
 function getDataInfo(filePath) {
   return fsPromises.readFile(filePath, { encoding: 'utf8' })
-    .then((data) => { JSON.parse(data); })
-    .catch((err) => { console.log(err); });
+    // eslint-disable-next-line arrow-body-style
+    .then((data) => { return JSON.parse(data); })
+    // eslint-disable-next-line arrow-body-style
+    .catch((err) => { return console.log(err); });
 }
 
 module.exports = getDataInfo;
